45a9dfee0711b1b8c5b09daa581456eef73930eb
cuongnk-2655 <nguyen.khac.cuong@sun-asterisk.com>
2021-08-18 23:02:44 +0700

    Chapter 11 Account activation

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,20 +1,30 @@
 class AuthsController < ApplicationController
+  before_action :load_user, only: %i(create)
+
   def new; end
 
   def create
-    user = User.find_by email: params[:auth][:email].downcase
-    if user&.authenticate params[:auth][:password]
-      log_in user
-      params[:auth][:remember_me] == Settings.remember ? remember(user) : forget(user)
-      redirect_to user
+    if @user.activated && @user.authenticate(params[:auth][:password])
+      log_in @user
+      params[:auth][:remember_me] == Settings.remember ? remember(@user) : forget(@user)
+      redirect_back_or @user
     else
-      flash.now[:danger] = t :invalid_email_password_combination
-      render :new
+      flash[:warning] = t :account_not_activate
+      redirect_to root_path
     end
   end
 
   def destroy
     log_out
     redirect_to login_path
+  end
+
+  private
+  def load_user
+    @user = User.find_by email: params[:auth][:email].downcase
+    return if @user
+
+    flash.now[:danger] = t :invalid_email_password_combination
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
@@ -8,6 +8,9 @@ class User < ApplicationRecord
     length: {maximum: Settings.email.max_length, minimum: Settings.email.min_length}
   validates :name, presence: true,
     length: {maximum: Settings.name.max_length, minimum: Settings.name.min_length}
+
+  before_save{email.downcase!}
+  before_create :create_activation_digest
 
   scope :sort_by_new, ->{order created_at: :desc}
 
@@ -37,7 +40,24 @@ class User < ApplicationRecord
     update_column :remember_digest, nil
   end
 
-  def authenticated? remember_token
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless digest
+
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
